9e8909ad3645400374ea4b583699709be1f0febf
csh0117 <tnsgur!053>
2019-12-15 10:48:40 +0900

    백버튼 이벤트 커스터마이징

--- app/src/main/java/com/tjeit/a20191215_01_lifecycle/SecondActivity.kt
+++ app/src/main/java/com/tjeit/a20191215_01_lifecycle/SecondActivity.kt
@@ -1,5 +1,7 @@
 package com.tjeit.a20191215_01_lifecycle
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 
 class SecondActivity : BaseActivity() {
@@ -10,11 +12,21 @@ class SecondActivity : BaseActivity() {
         setValue()
         setupEvent()
     }
-
-
     override fun setupEvent() {
     }
 
     override fun setValue() {
+    }
+
+    override fun onBackPressed() {
+//        super.onBackPressed()
+        val alert = AlertDialog.Builder(mContext)
+        alert.setTitle("게시글 작성 중지 확인")
+        alert.setMessage("정말 글 작성을 중단 하시겠습니까? 작성하신 내용은 모두 삭제됩니다.")
+        alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+            finish()
+        })
+        alert.setNegativeButton("취소", null)
+        alert.show()
     }
 }
