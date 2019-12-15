e6f91b74a679c17a6d8eb2f077213fe31407f4f8
csh0117 <tnsgur!053>
2019-12-15 10:44:30 +0900

    생명주기 관련 로그

--- app/src/main/java/com/tjeit/a20191215_01_lifecycle/MainActivity.kt
+++ app/src/main/java/com/tjeit/a20191215_01_lifecycle/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjeit.a20191215_01_lifecycle
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -9,10 +10,11 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setValue()
         setupEvent()
-    }
 
+        Log.d("log]생명주기","onCreate 실행")
+    }
 
     override fun setupEvent() {
         callActivityBtn.setOnClickListener {
@@ -22,5 +24,18 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValue() {
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("log]생명주기","onResume 실행")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d("log]생명주기","onPause 실행")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("log]생명주기","onDestroy 실행")
     }
 }
--- app/src/main/java/com/tjeit/a20191215_01_lifecycle/SecondActivity.kt
+++ app/src/main/java/com/tjeit/a20191215_01_lifecycle/SecondActivity.kt
@@ -7,7 +7,8 @@ class SecondActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-
+        setValue()
+        setupEvent()
     }
 
 
